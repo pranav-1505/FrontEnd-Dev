155b802ca6616302638771737bda15e246fd6ab1
Pranav Patil <pranavpatil655@gmail.com>
2025-04-01 12:14:52 +0530

    counter app
    this is a counter app in various react method like useState, useReducer, Context API + useReducer and Redux Toolkit

---
+++ react-counter-app/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import CounterUseState from './components/CounterUseState';
+
+function App() {
+
+  return (
+    <>
+    <CounterUseState />
+    </>
+  )
+}
+
+export default App
---
+++ react-counter-app/src/components/CounterUseState.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+
+function CounterUseState(){
+    const[count, setCount]=useState(0)
+
+    const increment = () =>setCount(count +1)
+    
+
+    const decrement = () => setCount(prevCount => Math.max(prevCount - 1, 0));
+
+    const Reset = () => setCount(0)
+    
+
+    return(
+<div className="counter-container">
+    <h1 className="counter-heading">Counter App Using UseState</h1>
+    <h1 className="counter-value">{count}</h1>
+    <button className="counter-button increment" onClick={increment}>+</button>
+    <button className="counter-button decrement" onClick={decrement}>-</button>
+    <button className="counter-button reset" onClick={Reset}>Reset</button>
+</div>
+    )
+}
+export default CounterUseState;
